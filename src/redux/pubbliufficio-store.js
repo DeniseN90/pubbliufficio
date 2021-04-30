d867a239c1d11bb1ce370ed2466d6525699bd552
Denise Nanni <denise@localhost.localdomain>
2021-04-30 23:03:23 +0200

    style and store

--- src/redux/pubbliufficio-store.js
+++ src/redux/pubbliufficio-store.js
@@ -3,23 +3,21 @@ import { createSlice } from '@reduxjs/toolkit'
 const pubbliufficioStore = createSlice({
   name: 'pubbliufficio',
   initialState: {
-      isMobile: /Mobi/i.test(window.navigator.userAgent)
+      isMobile: /Mobi/i.test(window.navigator.userAgent),
+      isMenuOpen: false,
+      isFormSubmitted: false,
+      formValues: null,
   },
   reducers: {
-    incremented: (state, action) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
-      state.value += 1
-      //action.payload;
+    setIsMenuOpen: (state, action) => {
+      state.isMenuOpen = action.payload;
     },
-    decremented: state => {
-      state.value -= 1
-    }
+    setIsFormSubmitted: (state, action) => {
+      state.isFormSubmitted = action.payload;
+    },
   }
 })
 
-export const { incremented, decremented } = pubbliufficioStore.actions
+export const { setIsMenuOpen, setIsFormSubmitted } = pubbliufficioStore.actions
 
 export default pubbliufficioStore.reducer
