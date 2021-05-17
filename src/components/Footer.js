ae1a68ffa9e342cb5c2575c03c72715afea41169
Denise Nanni <denise@localhost.localdomain>
2021-05-17 18:31:31 +0200

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-    return (
-        <div className='Footer container-fluid'>
-            <div className='row'>
-                <div className='col'>
-                </div>
-            </div>
-        </div>
-    )
+  return (
+    <div className="Footer container-fluid">
+      <div className='Copyright'>Copyright &#169;</div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
